57af5da59f2e1d97d5fd00b44d4501de60c5f506
mengrang <mengrang8969@gmail.com>
2019-02-22 11:21:45 +0800

    day11

--- 数组中的逆序对.py
+++ 数组中的逆序对.py
@@ -16,5 +16,39 @@
 	对于%100的数据,size<=2*10^5
 '''
 '''
-分析：归并排序
+分析：归并排序O(nlogn)
 树形数组
+'''
+class Solution():
+	#Merge-sort
+	def inverse_pairs(self, data):
+		p = 0
+		if len(data) <= 1:
+			return data, p
+		mid = len(data) // 2
+		left, p = self.inverse_pairs(data[:mid])
+		right, p = self.inverse_pairs(data[mid:])
+		return self.merge(left, right, p)
+
+	def merge(self, left, right, p):
+		i = j = 0
+		result = []
+		while i < len(left) and j < len(right):
+			if left[i] < right[j]:
+				result.append(left[i])
+				i += 1
+			else:
+				result.append(right[j])
+				j += 1
+				p += 1
+		result.extend(left[i:])
+		result.extend(right[j:])
+		return result, p
+
+data = [100,11,8,9,6,2,4,5,7,1,10,3]
+M = Solution()
+print(M.inverse_pairs(data))
+
+
+
+
